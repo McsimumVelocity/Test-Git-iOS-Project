12b85804c26b5553a5fdbacf300c297e057093b9
Mike McNamara <Mike@Mikes-MacBook-Air.local>
2014-07-04 11:40:23 +0100

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Mike McNamara on 04/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Mike McNamara on 04/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
