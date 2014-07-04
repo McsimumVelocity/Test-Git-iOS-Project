532dd389d44748f111057c1ef181c08d1583fd29
Mike McNamara <Mike@Mikes-MacBook-Air.local>
2014-07-04 10:56:14 +0100

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Mike McNamara on 04/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Mike McNamara on 04/07/2014.
+//  Copyright (c) 2014 Mike McNamara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
